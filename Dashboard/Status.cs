1c0a7f141901f9899d928297efd4418d2d939968
alexfridman <afridman0793@conestogac.on.ca>
2024-11-05 21:32:56 -0500

    objects initialized and integrated to GUI

---
+++ Dashboard/Status.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dashboard
+{
+    class Status
+    {
+        private string statusState = "OFFLINE"; // status indicator notifying if we're online by default offline
+
+        public string StatusState
+        {
+            get { return statusState; }
+            set { statusState = value; }
+        }
+    }
+}
